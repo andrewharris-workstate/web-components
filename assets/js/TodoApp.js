342084512adceea46efa9da9caba40cbfea7d5ba
Andrew Harris <aharri10@cscc.edu>
2019-09-30 00:07:52 -0400

    bunch of changes, localStorage, etc

--- assets/js/TodoApp.js
+++ assets/js/TodoApp.js
@@ -1,29 +1,77 @@
-import TodoItem from './TodoItem';
-import TodoForm from './TodoForm';
+import TodoItem from './TodoItem.js';
+import TodoForm from './TodoForm.js';
 
 export default class TodoApp extends HTMLElement {
 	constructor() {
 		super();
 
-		const template = document.getElementById('todo-list')
-			.content;
-		const shadowRoot = this.attachShadow({mode: 'open'})
-			.appendChild(template.cloneNode(true));
+		this.localStorage = window.localStorage;
+		const shadowRoot = this.attachShadow({ mode: 'open' });
+		const todos = this.getTodosFromLocalStorage();
+		
+		shadowRoot.innerHTML = /*html*/`
+			<div id="todo-list">
+				<h1>Todo List</h1>
+				<todo-form></todo-form>
+				<ul id="todos">
+					<slot name="todos">
+					${todos && todos.map((item, i) => /*html*/`
+						<todo-item todo-id="${i}">
+							<span slot="title">${item.title}</span>
+							<span slot="description">${item.description}</span>
+						</todo-item>
+					`).join('')}
+					</slot>
+				</ul>
+			</div>
+		`;
+		this.container = shadowRoot;
 		this.addTodo = this.addTodo.bind(this);
+		this.removeTodo = this.removeTodo.bind(this);
+		this.getTodosFromLocalStorage = this.getTodosFromLocalStorage.bind(this);
+		this.saveToLocalStorage = this.saveToLocalStorage.bind(this);
 	}
 
 	connectedCallback() {
-		this.shadowRoot.addEventListener('saveTodo', this.addTodo);
+		this.container.addEventListener('saveTodo', this.addTodo);
+		this.container.addEventListener('deleteTodo', this.removeTodo);
 	}
 
 	addTodo(e) {
 		const { detail } = e;
 		var li = document.createElement('todo-item');
-		li.innerHTML = `
+		li.setAttribute('todo-id', this.getTodosFromLocalStorage().length);
+		li.innerHTML = /*html*/`
 			<span slot="title">${detail.title}</span>
 			<span slot="description">${detail.description}</span>
 		`;
-		this.shadowRoot.querySelector('#todos').appendChild(li);
+		this.container.querySelector('#todos').querySelector('slot[name="todos"]').appendChild(li);
+		this.saveToLocalStorage(detail);
+	}
+
+	removeTodo(e) {
+		const { detail } = e;
+		const todos = this.getTodosFromLocalStorage();
+		const newTodos = todos.filter((item, i) => i.toString() !== detail.id);
+		this.localStorage.setItem('todos', JSON.stringify(newTodos));
+	}
+
+	getTodosFromLocalStorage() {
+		try {
+			const todos = JSON.parse(this.localStorage.getItem('todos'));
+
+			if (!Array.isArray(todos)) throw new Error;
+			
+			return todos;
+		} catch (e) {
+			return [];
+		}
+	}
+
+	saveToLocalStorage(item) {
+		const todos = this.getTodosFromLocalStorage();
+		todos.push(item);
+		this.localStorage.setItem('todos', JSON.stringify(todos));
 	}
 }
 
--- assets/js/TodoForm.js
+++ assets/js/TodoForm.js
@@ -2,21 +2,58 @@ export default class TodoForm extends HTMLElement {
 	constructor() {
 		super();
 
-		const template = document.getElementById('todo-form')
-			.content;
-		const shadowRoot = this.attachShadow({mode: 'open'})
-			.appendChild(template.cloneNode(true));
+		const shadowRoot = this.attachShadow({ mode: 'open' });
+		shadowRoot.innerHTML = /*html*/`
+			<style>
+				fieldset {
+					border: 1px solid #ccc;
+				}
+				fieldset > * {
+					box-sizing: border-box;
+					display: block;
+					margin: 1rem 0;
+					padding: 10px;
+					font: normal 16px Arial, sans-serif;
+				}
+				fieldset > input, fieldset > textarea {
+					width: 50%;
+					border: 1px solid #ccc;
+					border-radius: 3px;
+				}
+				fieldset > button {
+					background: #eee;
+					border: 1px solid #ccc;
+					padding: 5px 10px;
+				}
+			</style>
+			<div id="todo-form">
+				<fieldset>
+					<input type="text" id="title" placeholder="Title">
+					<textarea id="description" placeholder="Description"></textarea>
+					<button type="button" id="save-btn">Save</button>
+				</fieldset>
+			</div>
+		`;
+		this.titleInput = shadowRoot.querySelector('#title');
+		this.descInput = shadowRoot.querySelector('#description');
+		this.saveButton = shadowRoot.querySelector('#save-btn');
+		this.container = shadowRoot;
 		this.saveNewTodo = this.saveNewTodo.bind(this);
+		this.clearInputs = this.clearInputs.bind(this);
 	}
 
 	connectedCallback() {
-		this.shadowRoot.querySelector('#save-btn')
-			.addEventListener('click', this.saveNewTodo);
+		this.saveButton.addEventListener('click', this.saveNewTodo);
 	}
 
 	saveNewTodo() {
-		const title = this.shadowRoot.querySelector('#title').value || '';
-		const description = this.shadowRoot.querySelector('#description').value || '';
+		const title = this.titleInput.value || '';
+		const description = this.descInput.value || '';
+
+		if (title == '' || description == '') {
+			return false;
+		}
+
 		this.dispatchEvent(new CustomEvent('saveTodo', {
 			bubbles: true,
 			detail: {
@@ -24,6 +61,13 @@ export default class TodoForm extends HTMLElement {
 				description
 			}
 		}));
+
+		this.clearInputs();
+	}
+
+	clearInputs() {
+		this.titleInput.value = '';
+		this.descInput.value = '';
 	}
 }
 
